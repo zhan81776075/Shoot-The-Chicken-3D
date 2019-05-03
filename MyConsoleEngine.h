6bc3952144d01a515fca7b7ccc7838c1f2d9c129
zhan81776075 <416810161@qq.com>
2019-05-03 23:08:16 +0800

    update

---
+++ MyConsoleEngine.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <Windows.h>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <random>
+
+#include "Math.h"
+
+
+#define IS_KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
+#define IS_KEY_UP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
+#define DEBUG_MSG1(msg)	::MessageBoxA(0,msg,0,0);
+
+typedef unsigned char BYTE;
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"MyConsoleEngine.h"
+
+int main()
+{
+	Math::VECTOR2 vector2(2, 2);
+	std::cout<<vector2.Length();
+	return 0;
+}
